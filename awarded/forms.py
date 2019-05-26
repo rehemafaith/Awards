90cbde3a8f34cc5a16f362ba93717ed3f4663342
rehemafaith <rehemafaith01@gmail.com>
2019-05-27 00:04:43 +0300

    added a form for project submition

---
+++ awarded/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Profile,Project,Rating,Techused
+
+
+
+
+
+class ProjectForm(forms.ModelForm):
+   '''
+   New project creation form.
+   '''
+
+   name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Project Title'}))
+   description = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Description'}))
+   link = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Project Link'}))
+   description = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Project Description'}))
+
+   class Meta:
+      model = Project
+      fields = ['name','img','description','link','language']
--- awarded/views.py
+++ awarded/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Profile,Project
 
 
 def home(request):
     project = Project.objects.all()
 
-    return render(request, 'index.html',context)
+    return render(request, 'index.html',{'project':project})
 
+def new_project(request):
+
+   user = request.user
+   if request.method == 'POST':
+      form = ProjectForm(request.POST,request.FILES)
+      if form.is_valid():
+         project = form.save(commit=False)
+         project.author = user.profile
+         project.save()
+         return redirect('home')
+   else:
+      form = ProjectForm()
+
+   
+
+   return render(request, 'rate/new_project.html',{'form':form})
