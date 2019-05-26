93c0feda00b116028247e248112bbdacadf7df8d
rehemafaith <rehemafaith01@gmail.com>
2019-05-26 19:47:49 +0300

    added the database and  made the tables

--- awarded/models.py
+++ awarded/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+   '''
+   Holds user's profile data.
+   '''
+
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   avatar = models.ImageField(upload_to='avatars/',null=True,default='default.jpg')
+   bio = models.TextField(max_length=140,null=True)
+
+   def __str__(self):
+      return self.user.username
+
+class Techused(models.Model):
+   '''
+   Holds data for the programming languages used.
+   '''
+
+   techused = models.CharField(max_length=50)
+
+   def __str__(self):
+      return self.language
+
+class Project(models.Model):
+   '''
+   Model to hold user's project data.
+   '''
+
+   author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+   name = models.CharField(max_length=70)
+   description = models.TextField(max_length=140)
+   img = models.ImageField(upload_to='projects/')
+   link = models.CharField(max_length=140)
+   techused = models.ManyToManyField(Techused)
+   
+   def __str__(self):
+      return self.name
+      
+class Rating(models.Model):
+   '''
+   Model to hold projects' ratings
+   '''
+   review = models.CharField(max_length=140,default='')
+   design = models.IntegerField(default=0)
+   usability = models.IntegerField(default=0)
+   content = models.IntegerField(default=0)
+   rated = models.ForeignKey(Project, on_delete=models.CASCADE)
+   rated_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.rated.name
--- awarded/views.py
+++ awarded/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    project = Project.objects.all()
+
+    return render(request, 'index.html',context)
 
